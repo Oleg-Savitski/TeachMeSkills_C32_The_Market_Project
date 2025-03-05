ad448658a58b3100c09fb1bc77721da02ddb1b9c
Oleg-Savitski <zoogostinica2016@gmail.com>
2025-03-05 18:41:51 +0300

    new/update

--- src/main/java/com/teachmeskills/market/interceptor/LoggingInterceptor.java
+++ src/main/java/com/teachmeskills/market/interceptor/LoggingInterceptor.java
@@ -19,19 +19,23 @@ public class LoggingInterceptor implements HandlerInterceptor {
         logger.info("Request URL -> {}", request.getRequestURL());
         logger.info("Method -> {}", request.getMethod());
         logger.info("Remote Address -> {}", request.getRemoteAddr());
+        System.out.println("Request URL -> " + request.getRequestURL());
         return true;
     }
 
     @Override
     public void postHandle(@NonNull HttpServletRequest request, HttpServletResponse response, @NonNull Object handler, ModelAndView modelAndView){
         logger.info("Response Status -> {}", response.getStatus());
+        System.out.println("Response Status -> " + response.getStatus());
     }
 
     @Override
     public void afterCompletion(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,@NonNull Object handler, Exception ex){
         if (ex != null) {
             logger.error("Request failed with exception -> {}", ex.getMessage());
+            System.out.println("Request failed with exception -> {" + ex.getMessage());
         }
         logger.info("Request completed for URL -> {}", request.getRequestURL());
+        System.out.println("Request completed for URL -> " + request.getRequestURL());
     }
 }
