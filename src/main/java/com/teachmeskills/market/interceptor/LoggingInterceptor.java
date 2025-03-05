0920aa6e526bc8fc2d847f7f3192cb052c05b0cb
Oleg-Savitski <zoogostinica2016@gmail.com>
2025-03-05 18:04:27 +0300

    new/update

---
+++ src/main/java/com/teachmeskills/market/interceptor/LoggingInterceptor.java
@@ -0,0 +1,37 @@
+package com.teachmeskills.market.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component("log_views_interceptor")
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) {
+        logger.info("Request URL -> {}", request.getRequestURL());
+        logger.info("Method -> {}", request.getMethod());
+        logger.info("Remote Address -> {}", request.getRemoteAddr());
+        return true;
+    }
+
+    @Override
+    public void postHandle(@NonNull HttpServletRequest request, HttpServletResponse response, @NonNull Object handler, ModelAndView modelAndView){
+        logger.info("Response Status -> {}", response.getStatus());
+    }
+
+    @Override
+    public void afterCompletion(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,@NonNull Object handler, Exception ex){
+        if (ex != null) {
+            logger.error("Request failed with exception -> {}", ex.getMessage());
+        }
+        logger.info("Request completed for URL -> {}", request.getRequestURL());
+    }
+}
--- src/main/java/com/teachmeskills/market/utils/config/application/AppConfig.java
+++ src/main/java/com/teachmeskills/market/utils/config/application/AppConfig.java
@@ -1,16 +1,35 @@
 package com.teachmeskills.market.utils.config.application;
 
 import com.teachmeskills.market.annotation.LeadTimed;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+@EnableWebMvc
 @ComponentScan("com.teachmeskills.market")
 @Configuration
 @EnableAspectJAutoProxy
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
+
+    public HandlerInterceptor loggingInterceptor;
+
+    @Autowired
+    public AppConfig (@Qualifier(value = "log_views_interceptor")HandlerInterceptor loggingInterceptor) {
+        this.loggingInterceptor = loggingInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor).addPathPatterns("/**");
+    }
 
     @LeadTimed("-> Worked out method viewResolver")
     @Bean
